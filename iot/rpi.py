3600eaf88a7faf2d4b8f85d03f84159104a9525d
Mansikkapiiraka <amor27moni@gmail.com>
2019-03-06 16:34:37 +0200

    changed pin back to 4

--- iot/rpi.py
+++ iot/rpi.py
@@ -10,7 +10,7 @@ def readSensors():
     try:
         pin = os.environ["GPIO"]
     except:
-        pin = 2
+        pin = 4
     humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, pin)
     sensors = {
         "temperature": temperature,
